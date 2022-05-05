01114d2b0d5dbcd37fb531297cf224ac858a5efb
cschulz@nexible.de <cschulz@nexible.de>
2022-05-05 07:40:55 +0000

    updated_to_last_code_status_of_tutorial

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,12 +1,7 @@
-with payments as (
-    select amount, order_id as id from {{ ref('stg_payments') }} where status_payment = 'success'
-)
-
 select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status,
-    payments.amount
-from {{ source('jaffle_shop','orders')}}
-left join payments using(id)
+    status
+
+from {{ source('jaffle_shop', 'orders') }}
--- tests/assert_stg_payments_total_amount_greater_zero.sql
+++ tests/assert_stg_payments_total_amount_greater_zero.sql
@@ -6,7 +6,7 @@ payments as (
 
 select
     order_id,
-    sum(amount) as total_amount
+    sum(amount)
 from payments
 group by order_id
-having total_amount < 0
+having sum(amount) < 0
