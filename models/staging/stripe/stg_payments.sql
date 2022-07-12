8bbf865834effa363483d97319e50c7696c06977
cschulz@nexible.de <cschulz@nexible.de>
2022-07-12 06:16:59 +0000

    reorder fct_payments and stg_payments

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,9 @@
-select
+select 
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
-from {{ source('stripe', 'payment') }}
+    {{ cents_to_dollars('amount') }} as amount,
+    created as created_as
+from
+    {{ source('stripe', 'payment')}}
